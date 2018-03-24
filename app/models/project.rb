7e526b3b8b3753a7f8087c7e202780fd793dbe37
nikita_mullovsky <mullovskii@gmail.com>
2018-03-24 15:33:16 +0300

    statuses

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,7 +3,7 @@ class Project < ApplicationRecord
 	has_many :votes
 
 	enum status: [ 'Private sale', 'Pre-sale', 'Pre-sale завершен', 'Crowdsale', 'Development', 'Production', 'Exchange']
-	enum relation: [ 'Анализируем', 'Голосуем', 'Вошли', 'Отказали']
+	enum relation: [ 'Анализируем', 'Голосуем', 'Ждем хайпа', 'Пробуем войти', 'Вошли', 'Отказали']
 
 
 
